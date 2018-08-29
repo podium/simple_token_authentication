67cbf4d98f76609178c6d0a61bf69f6d9dd7aacb
Jason Turner <jasonalmaturner@gmail.com>
2018-08-29 10:10:14 -0600

    update deps

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SimpleTokenAuthentication.Mixfile do
   def project do
     [app: :simple_token_authentication,
      version: @version,
-     elixir: "~> 1.4",
+     elixir: "~> 1.6",
      description: description(),
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -32,7 +32,7 @@ defmodule SimpleTokenAuthentication.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:plug, "~> 1.3"},
+      {:plug, "~> 1.4"},
       {:credo, "~> 0.8", only: [:dev, :test]},
       {:ex_doc, ">= 0.16.0", only: :dev}
     ]
