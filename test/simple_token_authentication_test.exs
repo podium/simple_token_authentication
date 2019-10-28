8adaa23dc227fbb202f8971c62a0ec5b8bbe6a79
Brett Beatty <brettbeatty@gmail.com>
2019-10-28 11:02:37 -0600

    Use Plug.Crypto.secure_compare/2 to compare tokens
    Regular string compare opens us up to timing attacks

--- test/simple_token_authentication_test.exs
+++ test/simple_token_authentication_test.exs
@@ -5,27 +5,38 @@ defmodule SimpleTokenAuthenticationTest do
   @opts SimpleTokenAuthentication.init([])
 
   defmacro with_token(token, do: expression) do
-		quote do
-			Application.put_env(:simple_token_authentication, :token, unquote(token))
-			unquote(expression)
-			Application.put_env(:simple_token_authentication, :token, nil)
-		end
-	end
+    quote do
+      Application.put_env(:simple_token_authentication, :token, unquote(token))
+      unquote(expression)
+      Application.put_env(:simple_token_authentication, :token, nil)
+    end
+  end
 
   describe "without a token" do
-		test "returns a 401 status code" do
-			with_token(nil) do
-				# Create a test connection
-				conn = conn(:get, "/foo")
+    test "returns a 401 status code" do
+      with_token(nil) do
+        # Create a test connection
+        conn = conn(:get, "/foo")
 
-				# Invoke the plug
-				conn = SimpleTokenAuthentication.call(conn, @opts)
+        # Invoke the plug
+        conn = SimpleTokenAuthentication.call(conn, @opts)
 
-				# Assert the response and status
-				assert conn.status == 401
-			end
-		end
-	end
+        # Assert the response and status
+        assert conn.status == 401
+      end
+    end
+  end
+
+  test "handles the lack of an auth header" do
+    with_token("fake_token") do
+      conn =
+        :get
+        |> conn("/foo")
+        |> SimpleTokenAuthentication.call(@opts)
+
+      assert conn.status == 401
+    end
+  end
 
   describe "empty token" do
     test "returns a 401 status code" do
@@ -46,7 +57,7 @@ defmodule SimpleTokenAuthenticationTest do
   end
 
   describe "with an invalid token" do
-		test "returns a 401 status code" do
+    test "returns a 401 status code" do
       with_token("fake_token") do
         # Create a test connection
         conn =
@@ -60,11 +71,11 @@ defmodule SimpleTokenAuthenticationTest do
         # Assert the response and status
         assert conn.status == 401
       end
-		end
-	end
+    end
+  end
 
   describe "with a valid token" do
-		test "returns a 200 status code" do
+    test "returns a 200 status code" do
       with_token("fake_token") do
         # Create a test connection
         conn =
@@ -78,6 +89,6 @@ defmodule SimpleTokenAuthenticationTest do
         # Assert the response and status
         assert conn.status != 401
       end
-		end
-	end
+    end
+  end
 end
