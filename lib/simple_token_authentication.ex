763acf2198a1f0573a6e4470a3a4b04b12871855
Trevor Fenn <sgtpepper43@gmail.com>
2019-12-17 15:09:56 -0700

    Accept list of tokens

--- lib/simple_token_authentication.ex
+++ lib/simple_token_authentication.ex
@@ -9,17 +9,21 @@ defmodule SimpleTokenAuthentication do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    token = Application.get_env(:simple_token_authentication, :token)
-
     val = get_auth_header(conn)
 
-    if token && String.trim(token) != "" && secure_compare(token, val) do
+    token_matches? =
+      :simple_token_authentication
+      |> Application.get_env(:token)
+      |> List.wrap()
+      |> Enum.any?(&matches?(&1, val))
+
+    if token_matches? do
       conn
     else
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(401, ~s({ "error": "Invalid shared key" }))
-      |> halt
+      |> halt()
     end
   end
 
@@ -29,4 +33,9 @@ defmodule SimpleTokenAuthentication do
       _ -> ""
     end
   end
+
+  defp matches?(token, value) when is_binary(token) and is_binary(value), do: String.trim(token) != "" && secure_compare(token, value)
+
+  defp matches?(_, _), do: false
+
 end
